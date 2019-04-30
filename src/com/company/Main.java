9e4c2a4b2a9b2adffbd543846b58903c9adee6b0
Justin Lazarski <blackhawks189@gmail.com>
2019-04-29 23:37:04 -0700

    Initial commit.

---
+++ src/com/company/Main.java
@@ -0,0 +1,23 @@
+//Justin Lazarski
+//4/29/19
+//Reverse a string
+package com.company;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter a string:");
+
+        String userString = input.next();
+        String reverseString = "";
+
+        userString = userString.toLowerCase();
+        System.out.println(userString);
+
+        reverseString = new StringBuilder(userString).reverse().toString();
+        System.out.println(reverseString);
+    }
+}
